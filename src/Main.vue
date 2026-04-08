3bc99af15138328edd2fa721d08310b96fcbc523
heyManNice <3031978990@qq.com>
2026-04-08 15:22:08 +0800

    style

--- src/Main.vue
+++ src/Main.vue
@@ -1,12 +1,22 @@
 <template>
-    <!-- 边栏 -->
-    <n>
-        <!-- 头像 -->
-        <n></n>
-        <!-- 操作按钮区 -->
-        <n></n>
-        <!-- 菜单按钮区 -->
-        <n></n>
+    <!-- 总容器 -->
+    <n class="flex flex-row h-screen">
+        <!-- 边栏导航 -->
+        <n class="w-15">
+            <!-- 头像 -->
+            <n></n>
+            <!-- 操作按钮区 -->
+            <n></n>
+            <!-- 菜单按钮区 -->
+            <n></n>
+        </n>
+        <!-- 导航内容区 -->
+        <n class="flex-1 flex flex-row">
+            <!-- 导航列表区 -->
+            <n class="w-60"></n>
+            <!-- 导航内容区 -->
+            <n class="flex-1"></n>
+        </n>
     </n>
 </template>
 <script lang="ts" setup>
