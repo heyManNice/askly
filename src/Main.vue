309e5182567e9c4f4ad7ec8db80e34886c7895d1
heyManNice <3031978990@qq.com>
2026-04-08 15:30:06 +0800

    style

--- src/Main.vue
+++ src/Main.vue
@@ -2,13 +2,13 @@
     <!-- 总容器 -->
     <n class="flex flex-row h-screen">
         <!-- 边栏导航 -->
-        <n class="w-15">
+        <n class="w-15 shrink-0 flex flex-col gap-2">
             <!-- 头像 -->
-            <n></n>
+            <n>1</n>
             <!-- 操作按钮区 -->
-            <n></n>
+            <n class="flex-1">2</n>
             <!-- 菜单按钮区 -->
-            <n></n>
+            <n>3</n>
         </n>
         <!-- 导航内容区 -->
         <n class="flex-1 flex flex-row">
