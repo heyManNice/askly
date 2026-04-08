b5d7e6e36e92bab6c1febf710a98c821913afab1
heyManNice <3031978990@qq.com>
2026-04-08 15:21:48 +0800

    chore：vue排除n标签

--- vite.config.ts
+++ vite.config.ts
@@ -4,7 +4,13 @@ import tailwindcss from '@tailwindcss/vite';
 
 export default defineConfig({
     plugins: [
-        vue(),
+        vue({
+            template: {
+                compilerOptions: {
+                    isCustomElement: (tag) => tag === 'n'
+                }
+            }
+        }),
         tailwindcss(),
     ],
 });
