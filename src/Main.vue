81a9a3a3e60f1190ff946393c0f20167a7fba26a
heyManNice <3031978990@qq.com>
2026-04-08 15:04:02 +0800

    chore：添加n标签

--- src/Main.vue
+++ src/Main.vue
@@ -1,5 +1,14 @@
 <template>
-    <n class="text-3xl font-bold underline text-green-300">
-        Hello world!
+    <!-- 边栏 -->
+    <n>
+        <!-- 头像 -->
+        <n></n>
+        <!-- 操作按钮区 -->
+        <n></n>
+        <!-- 菜单按钮区 -->
+        <n></n>
     </n>
 </template>
+<script lang="ts" setup>
+
+</script>
