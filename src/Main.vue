bc420ced991cc2cd0a190a300be9a314b7b62f95
heyManNice <3031978990@qq.com>
2026-04-09 16:03:47 +0800

    style

--- src/Main.vue
+++ src/Main.vue
@@ -1,6 +1,6 @@
 <template>
     <!-- 总容器 -->
-    <n class="flex flex-row h-screen">
+    <n class="flex flex-row h-screen gap-2">
         <!-- 边栏导航 -->
         <n class="w-15 shrink-0 flex flex-col gap-5 items-center pb-5">
             <!-- 头像 -->
@@ -18,11 +18,11 @@
             </n>
         </n>
         <!-- 导航内容区 -->
-        <n class="flex-1 flex flex-row">
+        <n class="flex-1 flex flex-row gap-2">
             <!-- 导航列表区上下分栏 -->
             <n class="w-60 flex flex-col gap-2">
                 <!-- 搜索框 -->
-                <n class="h-8 flex px-2">
+                <n class="h-8 flex">
                     <input class="flex-1 px-2 bg-gray-100 rounded" type="text" placeholder="搜索">
                 </n>
                 <!-- 列表区域 -->
