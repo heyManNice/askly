f8cd0049ac6288c2445991d9118a3796a3c832c2
heyManNice <3197871458@qq.com>
2026-04-12 02:57:42 +0800

    refactor：修改手机导航栏位置

--- src/Main.vue
+++ src/Main.vue
@@ -1,110 +1,115 @@
 <template>
-    <!-- 总容器 -->
-    <n class="flex flex-row h-screen">
-        <!-- 边栏导航 -->
-        <n class="w-15 shrink-0 flex-col gap-3 items-center pb-2 md:border-r border-gray-200 hidden sm:flex">
-            <!-- 头像 -->
-            <img class="w-10 rounded" :src="avatar" alt="avatar">
-            <!-- 操作按钮区 -->
-            <n class="flex-1 flex flex-col gap-1">
-                <template v-for="route in routes" :key="route.name">
-                    <!-- 外面的包围样式 -->
-                    <n class="p-2 rounded hover:bg-gray-100" :class="{
-                        'bg-gray-100': route.name === 'conversations',
-                    }">
-                        <!-- 里面的图标 -->
-                        <component :is="route.icon" class="cursor-pointer" />
-                    </n>
-                </template>
+    <!-- 手机导航栏容器 -->
+    <n class="flex flex-col h-screen">
+        <!-- 电脑版导航栏容器 -->
+        <n class="flex flex-row h-screen">
+            <!-- 边栏导航 -->
+            <n class="w-15 shrink-0 flex-col gap-3 items-center pb-2 md:border-r border-gray-200 hidden sm:flex">
+                <!-- 头像 -->
+                <img class="w-10 rounded" :src="avatar" alt="avatar">
+                <!-- 操作按钮区 -->
+                <n class="flex-1 flex flex-col gap-1">
+                    <template v-for="route in routes" :key="route.name">
+                        <!-- 外面的包围样式 -->
+                        <n class="p-2 rounded hover:bg-gray-100" :class="{
+                            'bg-gray-100': route.name === 'conversations',
+                        }">
+                            <!-- 里面的图标 -->
+                            <component :is="route.icon" class="cursor-pointer" />
+                        </n>
+                    </template>
+                </n>
+                <!-- 菜单按钮区 -->
+                <n class="p-2 rounded hover:bg-gray-100">
+                    <FiMenu class="cursor-pointer" />
+                </n>
             </n>
-            <!-- 菜单按钮区 -->
-            <n class="p-2 rounded hover:bg-gray-100">
-                <FiMenu class="cursor-pointer" />
+            <!-- 导航页面区 -->
+            <n class="flex-1 flex flex-row relative">
+                <!-- 功能列表区 -->
+                <n class="w-full h-full md:w-60 flex flex-col gap-2 px-2 md:static absolute">
+                    <!-- 搜索框 -->
+                    <n class="h-8 flex shrink-0">
+                        <input class="flex-1 px-2 bg-gray-100 rounded" type="text" placeholder="搜索">
+                    </n>
+                    <!-- 列表区域 -->
+                    <n class="flex-1 flex flex-col gap-2 overflow-y-auto">
+                        <!-- 列表卡片 -->
+                        <n v-for="conversation in conversations" :key="conversation.id"
+                            @click="isShowConversation = true"
+                            class="flex items-center rounded cursor-pointer hover:bg-gray-100 gap-2">
+                            <!-- 左边头像 -->
+                            <img class="w-10 rounded" :src="conversation.avatar" alt="avatar">
+                            <!-- 右边文字 -->
+                            <n class="flex flex-col flex-1 overflow-hidden">
+                                <!-- 标题和时间 -->
+                                <n class="flex items-center gap-2">
+                                    <!-- 标题 -->
+                                    <n class="font-medium mr-auto truncate">
+                                        {{ conversation.title }}
+                                    </n>
+                                    <!-- 更新时间 -->
+                                    <n class="text-xs text-gray-500">{{
+                                        conversation.updatedAt }}
+                                    </n>
+                                </n>
+                                <!-- 会话信息 -->
+                                <n class="text-sm text-gray-500 truncate">
+                                    {{ conversation.lastMessage }}
+                                </n>
+                            </n>
+                        </n>
+                    </n>
+                </n>
+                <!-- 导航内容区 -->
+                <transition :name="screenWidth < 768 ? 'slide-x' : 'disable'">
+                    <n v-if="isShowConversation"
+                        class="flex-1 flex flex-col h-screen md:h-full z-50 border-l bg-white border-gray-200 absolute md:static w-full">
+                        <!-- 标题栏 -->
+                        <n class="flex flex-row px-2 pb-2 border-b border-gray-200">
+                            <!-- mr5是为了让标题居中 -->
+                            <FiChevronLeft @click="isShowConversation = false"
+                                class="w-5 cursor-pointer hover:bg-gray-100 rounded block md:hidden mr-5" />
+                            <!-- 标题文字 -->
+                            <n class="mr-auto ml-auto md:ml-0">猫小咪</n>
+                            <!-- 按钮操作区 -->
+                            <n class="mr-2 flex gap-2">
+                                <VscPin class="w-5 cursor-pointer hover:bg-gray-100 rounded" />
+                                <FiMoreHorizontal class="w-5 cursor-pointer hover:bg-gray-100 rounded" />
+                            </n>
+                        </n>
+                        <!-- 信息显示区域 -->
+                        <n class="flex-1"></n>
+                        <!-- 信息输入框 -->
+                        <n class="h-40 flex flex-col shrink-0 border-t border-gray-200 px-2 py-1 gap-1">
+                            <!-- 功能选择区 -->
+                            <n class="flex flex-row gap-2">
+                                <FiFolder class="w-5 cursor-pointer hover:bg-gray-100 rounded" />
+                                <FiImage class="w-5 cursor-pointer hover:bg-gray-100 rounded" />
+                            </n>
+                            <!-- 输入框 -->
+                            <textarea class="flex-1 resize-none" type="text"
+                                placeholder="输入你的问题，按 Enter 发生，按 Shift + Enter 换行。" />
+                        </n>
+                    </n>
+                </transition>
             </n>
         </n>
-        <!-- 导航页面区 -->
-        <n class="flex-1 flex flex-row relative">
-            <!-- 功能列表区 -->
-            <n class="w-full h-full md:w-60 flex flex-col gap-2 px-2 md:static absolute">
-                <!-- 搜索框 -->
-                <n class="h-8 flex shrink-0">
-                    <input class="flex-1 px-2 bg-gray-100 rounded" type="text" placeholder="搜索">
-                </n>
-                <!-- 列表区域 -->
-                <n class="flex-1 flex flex-col gap-2">
-                    <!-- 列表卡片 -->
-                    <n v-for="conversation in conversations" :key="conversation.id" @click="isShowConversation = true"
-                        class="flex items-center rounded cursor-pointer hover:bg-gray-100 gap-2">
-                        <!-- 左边头像 -->
-                        <img class="w-10 rounded" :src="conversation.avatar" alt="avatar">
-                        <!-- 右边文字 -->
-                        <n class="flex flex-col flex-1 overflow-hidden">
-                            <!-- 标题和时间 -->
-                            <n class="flex items-center gap-2">
-                                <!-- 标题 -->
-                                <n class="font-medium mr-auto truncate">
-                                    {{ conversation.title }}
-                                </n>
-                                <!-- 更新时间 -->
-                                <n class="text-xs text-gray-500">{{
-                                    conversation.updatedAt }}
-                                </n>
-                            </n>
-                            <!-- 会话信息 -->
-                            <n class="text-sm text-gray-500 truncate">
-                                {{ conversation.lastMessage }}
-                            </n>
-                        </n>
-                    </n>
-                </n>
-                <!-- 手机版底部的bar导航 -->
-                <n class="sm:hidden flex flex-row gap-2 p-2 border-t border-gray-200 justify-between px-10 pt-2 pb-1">
-                    <n class="flex flex-col items-center">
-                        <FiMessageSquare />
-                        <n class="text-xs">会话</n>
-                    </n>
-                    <n class="flex flex-col items-center">
-                        <FiUsers />
-                        <n class="text-xs">角色</n>
-                    </n>
-                    <n class="flex flex-col items-center">
-                        <FiMenu />
-                        <n class="text-xs">更多</n>
-                    </n>
-                </n>
+        <!-- 手机版底部的bar导航 -->
+        <n
+            class="sm:hidden flex z-40 bg-white flex-row gap-2 p-2 border-t border-gray-200 justify-between px-10 pt-2 pb-1">
+            <n class="flex flex-col items-center">
+                <FiMessageSquare />
+                <n class="text-xs">会话</n>
             </n>
-            <!-- 导航内容区 -->
-            <transition :name="screenWidth < 768 ? 'slide-x' : 'disable'">
-                <n v-if="isShowConversation"
-                    class="flex-1 flex flex-col h-full border-l bg-white border-gray-200 absolute md:static w-full">
-                    <!-- 标题栏 -->
-                    <n class="flex flex-row px-2 pb-2 border-b border-gray-200">
-                        <!-- mr5是为了让标题居中 -->
-                        <FiChevronLeft @click="isShowConversation = false"
-                            class="w-5 cursor-pointer hover:bg-gray-100 rounded block md:hidden mr-5" />
-                        <!-- 标题文字 -->
-                        <n class="mr-auto ml-auto md:ml-0">猫小咪</n>
-                        <!-- 按钮操作区 -->
-                        <n class="mr-2 flex gap-2">
-                            <VscPin class="w-5 cursor-pointer hover:bg-gray-100 rounded" />
-                            <FiMoreHorizontal class="w-5 cursor-pointer hover:bg-gray-100 rounded" />
-                        </n>
-                    </n>
-                    <!-- 信息显示区域 -->
-                    <n class="flex-1"></n>
-                    <!-- 信息输入框 -->
-                    <n class="h-40 flex flex-col shrink-0 border-t border-gray-200 px-2 py-1 gap-1">
-                        <!-- 功能选择区 -->
-                        <n class="flex flex-row gap-2">
-                            <FiFolder class="w-5 cursor-pointer hover:bg-gray-100 rounded" />
-                            <FiImage class="w-5 cursor-pointer hover:bg-gray-100 rounded" />
-                        </n>
-                        <!-- 输入框 -->
-                        <textarea class="flex-1 resize-none" type="text"
-                            placeholder="输入你的问题，按 Enter 发生，按 Shift + Enter 换行。" />
-                    </n>
-                </n>
-            </transition>
+            <n class="flex flex-col items-center">
+                <FiUsers />
+                <n class="text-xs">角色</n>
+            </n>
+            <n class="flex flex-col items-center">
+                <FiMenu />
+                <n class="text-xs">更多</n>
+            </n>
         </n>
     </n>
 </template>
