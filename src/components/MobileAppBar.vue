5fd516778716c79e160984b9366565d7bc0731b8
heyManNice <3197871458@qq.com>
2026-04-13 17:06:02 +0800

    style

--- src/components/MobileAppBar.vue
+++ src/components/MobileAppBar.vue
@@ -1,24 +1,16 @@
 <template>
-    <n v-if="pufferStore.morph === 'compact'" @mousedown="handleMouseDown"
-        class="flex flex-row px-2 pb-2 border-gray-200">
+    <n v-if="pufferStore.morph !== 'expanded'" @mousedown="handleMouseDown"
+        class="flex flex-row px-2 pb-2 border-gray-200 pr-10">
         <!-- 左边返回图标 -->
         <FiChevronLeft @mousedown.stop @click="pageController.toTopPage()"
             class="w-5 cursor-pointer hover:bg-gray-100 rounded block" />
         <!-- 标题内容 -->
         <n class="mr-auto ml-auto">二级页面</n>
-        <!-- 会话操作按钮 -->
-        <n @mousedown.stop class="mr-2 flex gap-2">
-            <!-- 更多选项 -->
-            <n title="更多">
-                <FiMoreHorizontal class="w-5 cursor-pointer hover:bg-gray-100 rounded" />
-            </n>
-        </n>
     </n>
 </template>
 <script lang="ts" setup>
 import windowm from '@eziapp-org/bridge/windowm';
 import {
-    FiMoreHorizontal,
     FiChevronLeft
 } from 'vue-icons-plus/fi';
 
