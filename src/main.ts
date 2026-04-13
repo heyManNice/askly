e66ff79e6a6671fef8e0cd754c54cc6f8114c753
heyManNice <3197871458@qq.com>
2026-04-13 17:56:07 +0800

    style

--- src/main.ts
+++ src/main.ts
@@ -1,20 +1,18 @@
 import { createApp } from "vue";
 import { createPinia } from "pinia";
 import { cssXraySetup } from "@utils/cssXray";
+import "@src/main.css";
 
 import Main from "@src/Main.vue";
 import diffHtmlDirective from "@directives/diffHtml";
-import version from "@eziapp-org/bridge/version"
-import "@src/main.css";
 
 const app = createApp(Main);
-
 app.use(createPinia());
 app.directive("diff-html", diffHtmlDirective);
-
 app.mount(document.body);
 
-if (version.isDebug()) {
+// 启用这个能使用树摇优化
+if (import.meta.env.DEV) {
     cssXraySetup({
         shortcut: "shift+d"
     });
