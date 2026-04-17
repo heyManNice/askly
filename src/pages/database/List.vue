22aec2da9cc4c7cd93255c5d9d9d062677f1906e
heyManNice <3197871458@qq.com>
2026-04-18 02:22:02 +0800

    style

--- src/pages/database/List.vue
+++ src/pages/database/List.vue
@@ -1,10 +1,11 @@
 <template>
-    <n class="flex-1 flex flex-col h-full">
+    <n class="flex-1 flex flex-col h-full bg-white dark:bg-black">
+        <MobileAppBar title="数据库" />
         <n class="flex-1">数据库</n>
         <mobile-nav />
     </n>
 </template>
 <script lang="ts" setup>
-
+import MobileAppBar from '@components/MobileAppBar.vue';
 import MobileNav from '@components/MobileNav.vue';
 </script>
--- src/pages/resources/List.vue
+++ src/pages/resources/List.vue
@@ -1,10 +1,11 @@
 <template>
-    <n class="flex-1 flex flex-col h-full">
+    <n class="flex-1 flex flex-col h-full bg-white dark:bg-black">
+        <MobileAppBar title="资源" />
         <n class="flex-1">资源</n>
         <mobile-nav />
     </n>
 </template>
 <script lang="ts" setup>
-
+import MobileAppBar from '@components/MobileAppBar.vue';
 import MobileNav from '@components/MobileNav.vue';
 </script>
