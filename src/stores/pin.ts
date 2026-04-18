0784253f70fbe046808eb9d416b6e665743ce857
heyManNice <3197871458@qq.com>
2026-04-18 14:36:36 +0800

    feat：窗口置顶功能

--- src/stores/pin.ts
+++ src/stores/pin.ts
@@ -5,12 +5,21 @@ import {
     ref
 } from "vue";
 
+import windowm from '@eziapp-org/bridge/windowm';
+
 
 export const usePinStore = defineStore('pin', () => {
     const isPinned = ref(false);
+    let window: Awaited<ReturnType<typeof windowm.getCurrentWindow>> | null = null;
+    windowm.getCurrentWindow().then(w => {
+        window = w;
+    });
 
     function togglePin() {
         isPinned.value = !isPinned.value;
+        if (window) {
+            window.setAlwaysOnTop(isPinned.value)
+        }
     }
     return {
         isPinned,
