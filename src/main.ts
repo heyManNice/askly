098c7f925c3296a4af494be5485ee7da1d0c45cc
heyManNice <3197871458@qq.com>
2026-04-18 16:55:18 +0800

    feat：注册快捷键

--- src/main.ts
+++ src/main.ts
@@ -8,6 +8,7 @@ import Main from "@src/Main.vue";
 import diffHtmlDirective from "@directives/diffHtml";
 import tray from "@eziapp-org/bridge/tray";
 import windowm from "@eziapp-org/bridge/windowm";
+import shortcut from "@eziapp-org/bridge/shortcut";
 
 const app = createApp(Main);
 app.use(createPinia());
@@ -21,7 +22,20 @@ if (import.meta.env.DEV) {
     });
 }
 
-windowm.getCurrentWindow().then(w => {
+windowm.getCurrentWindow().then(win => {
     // 显示托盘图标
-    tray.show(w);
+    tray.show(win);
+
+    // 注册全局快捷键
+    shortcut.register({
+        alt: true,
+        key: "C",
+    }, async() => {
+        if(await win.isFocused()) {
+            await win.hide();
+        }else{
+            await win.show();
+            await win.focus();
+        }
+    });
 })
