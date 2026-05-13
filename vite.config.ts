2408563e4ee0140c90364b4ebce12ef2b16170db
heyManNice <3197871458@qq.com>
2026-05-13 18:04:34 +0800

    fix：__dirname

--- vite.config.ts
+++ vite.config.ts
@@ -2,6 +2,7 @@ import { defineConfig } from 'vite';
 import vue from '@vitejs/plugin-vue';
 import tailwindcss from '@tailwindcss/vite';
 import path from 'node:path';
+const __dirname = import.meta.dirname;
 
 export default defineConfig({
     resolve: {
